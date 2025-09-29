2ce54279e22225ea579f68d5048c7a5c22effbe7
Dualate <cmgfranklin@gmail.com>
2025-09-29 08:32:27 -0400

    basic jump implemented

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,18 +7,39 @@ public class Cube : MonoBehaviour
 {
     PlayerControls controls;
     public float moveSpeed;
+    public float jumpForce;
+    public bool grounded;
     float xSpeed;
     float ySpeed;
-    Vector2 moveVector; 
+    Vector2 moveVector;
     public void PLEASEGOTLETTHISWORK(CallbackContext context)
     {
 
         moveVector = context.ReadValue<Vector2>();
-        
+
     }
 
-    void Update(){
+    void Update()
+    {
         xSpeed = moveVector.x * moveSpeed * Time.deltaTime;
         transform.Translate(xSpeed, ySpeed, 0);
+    }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            grounded = false;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }   
     }
 }
