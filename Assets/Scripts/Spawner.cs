030ccd425fd115d37ad66aa52607944211bfcb66
Dualate <cmgfranklin@gmail.com>
2026-03-04 23:47:28 -0500

    cracked pathing (random)

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,17 +7,21 @@ public class Spawner : MonoBehaviour
     enum STATE { DORMANT, ACTIVE }
     STATE state = STATE.DORMANT;
     public float moveSpeed;
+    public float xMoveSpeed;
+
     public GameObject[] platform;
     public GameObject finishLine;
     int sent = 0;
     int chunk = 0;
-    int xScal = 1;
+    float xScal = 1;
     int yScal = 1;
+    bool spawning = true;
+    int chunks = 0;
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("SpawnPlatform", .15f, .1f);
-
+        SpawnChunk();
+        chunks = Random.Range(5, 10);
     }
 
     // Update is called once per frame
@@ -27,21 +31,65 @@ public class Spawner : MonoBehaviour
         float xVariation = 3f * Random.Range(-3, 4);
         Instantiate(platform[index], transform.position + new Vector3(xVariation, 0, 0), platform[index].transform.rotation);
         sent++;
-        if (sent == 50)
-        {
-            Instantiate(finishLine, transform.position + new Vector3(0,7,0), Quaternion.identity);
-        }
+
     }
 
 
     void Update()
     {
+        if (spawning)
+        {
+            transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * moveSpeed);
+            transform.Translate(new Vector3(xScal, 0, 0) * Time.deltaTime * xMoveSpeed);
+        }
 
-       transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
-       if (sent == 50)
-       {
+        if (sent == chunk)
+        {
             CancelInvoke("SpawnPlatform");
-            moveSpeed = 0;
-       }
+            if (chunks == 0)
+            {
+                spawning = false;
+                Instantiate(finishLine, transform.position + new Vector3(0, 7, 0), Quaternion.identity);
+                EndSpawn();
+            }
+            else
+            {
+                chunks -= 1;
+                sent = 0;
+                SpawnChunk();
+            }
+
+        }
+    }
+
+    void SpawnChunk()
+    {
+        int[] chunk_x = { -2, 0, 2 };
+        float repeatRate = 0;
+        chunk = Random.Range(1, 10);
+        xScal = chunk_x[Random.Range(0, chunk_x.Length)];
+        Debug.Log(xScal);
+        if (xScal < 0)
+        {
+            moveSpeed = 15;
+            repeatRate = .3f;
+        }
+        else if (xScal == 0)
+        {
+            moveSpeed = 45;
+            repeatRate = .1f;
+        }
+        else if (xScal > 0)
+        {
+            moveSpeed = 15;
+            repeatRate = .3f;
+        }
+
+        InvokeRepeating("SpawnPlatform", .15f, repeatRate);
+    }
+
+    void EndSpawn()
+    {
+
     }
 }
